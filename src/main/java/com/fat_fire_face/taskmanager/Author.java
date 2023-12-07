f0c03c2de8c1d534daa13be1a4dedd5906e8c000
Dmitriy <shurko-2003@yandex.ru>
2023-12-07 15:05:41 +0300

    added validation for email adress

--- src/main/java/com/fat_fire_face/taskmanager/Author.java
+++ src/main/java/com/fat_fire_face/taskmanager/Author.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 @Entity
 public class Author {
 
@@ -11,6 +15,12 @@ public class Author {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    //скорректировать по требованиям валидации
+    final int EMAIL_MIN_LENGTH = 3;
+    final int EMAIL_MAX_LENGTH = 320;
+   @NotBlank(message = "поле не может быть пустым")
+   @Size(min = EMAIL_MIN_LENGTH, max = EMAIL_MAX_LENGTH, message = "адрес электорнной почты должен содержать от 3 до 320 символов")
+   @Email(message = "адрес электронной почты должен содержать локальное имя, символ @ и доменное имя. Например, user123!@example.com")
     private String username;
     private String password;
 
